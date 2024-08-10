9517cd78db31c2bcb4e52fceefc28f6eefab26df
Utkarsh0826 <utkarshrajawat26@gmail.com>
2024-08-10 11:21:02 +0530

    L2E Backend Started

---
+++ src/main/java/com/learntoearn/learntoearn/LearntoearnApplication.java
@@ -0,0 +1,13 @@
+package com.learntoearn.learntoearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearntoearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearntoearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learntoearn/learntoearn/LearntoearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learntoearn.learntoearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearntoearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
